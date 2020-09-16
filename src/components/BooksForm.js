539166badc983f8336d2e8d1c196974ffd963a90
OdongAlican <sandieo.2020@gmail.com>
2020-09-17 02:01:12 +0300

    add handlechange event

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -1,20 +1,44 @@
 import React from 'react'
 
-const BooksForm = () => {
+class BooksForm extends React.Component {
+
+    constructor(props){
+
+        super(props)
+
+        this.state = {
+            title: '',
+            category: 'Action',
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(event){
+        this.setState({
+            [event.target.name]: event.target.value,
+        })
+
+    }
+
+
+    render(){
 
     const categories = ["Action", "Biography", "History", "Horror", "Kids", "Learning", "Sci-Fi"]
-
+    const { title, category } = this.state
+    
     return(
-       <form>
-           <input placeholder="Enter Book Title" type="text"/>
-           <select>
-               { categories.map(category => (
-                   <option key={category}>{ category }</option>
-               ))}
-            </select> 
-            <button type="submit">Submit</button>
-       </form> 
-    )
+        <form>
+            <input name="title" value = { title } placeholder="Enter Book Title" type="text" onChange={this.handleChange}/>
+            <select name="category" value = { category } onChange={this.handleChange}>
+                { categories.map(category => (
+                    <option key={category}>{ category }</option>
+                ))}
+             </select> 
+             <button type="submit">Submit</button>
+        </form> 
+     )
+    }
 }
 
 
